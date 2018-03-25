7baa4c01e93079c61df66c298e7e7cc4a4d0832d
vedemandi <35808123+vedemandi@users.noreply.github.com>
2018-03-25 17:04:11 -0400

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    MessagesComponent,
+    HomeComponent,
+    GameComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: 'home', component: HomeComponent },
+        { path: 'game', component: GameComponent },
+        { path: '', redirectTo: '/home', pathMatch: 'full'}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
