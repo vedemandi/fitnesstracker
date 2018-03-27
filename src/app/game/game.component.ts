e0b9015dc3d43ad96902dfd00ea1490608770b97
unknown <ved.emandi7@gmail.com>
2018-03-27 15:32:51 -0400

    tweaks

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,25 +1,34 @@
-import {Component,OnInit} from '@angular/core';
-import {game} from '../../models/game';
-import { User } from '../../models/game';
- @Component({
-     selector:'app-game',
-     templateUrl:'./game.component.html',
-     styleUrls:['./game.component.css']
- })
- export class GameComponent implements OnInit {
-     Model=new game();
-     Me=new User
-     constructor() {}
-     this.Me.Name="Moshe Plotkin"
-     ngOnInit() {
-     }
-     submitQuote(e: MouseEvent,Text: string,Playername:string) {
-     e.preventDefault();
-     if(this.MyPlayedQuote()) return;
-     this.Model.playquote.push({Text:text,PlayerName:playername,Chosen:false});
-     this.Model.MyQuotes.splice(this.Model.Myquotes.indexof(text),1)
-     }
-     MyPlayedQuote():Quote | null {
-         return this.Model.PlayedQuotes.find(x => x.Playername==this.Me.Name);
-     }
- }
+import { Component, OnInit } from '@angular/core';
+import { Game, User, Quote } from '../models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+    Model = new Game();
+    Me = new User();
+
+  constructor() {
+    this.Me.Name = "Moshe Plotkin"
+  }
+
+  ngOnInit() {
+  }
+
+  submitQuote(e: MouseEvent, text: string){
+    e.preventDefault();
+
+    if(this.MyPlayedQuote()) return;
+
+    this.Model.PlayedQuotes.push({ Text: text, PlayerName: this.Me.Name, Chosen: false });
+    this.Model.MyQuotes.splice( this.Model.MyQuotes.indexOf(text), 1 );
+  }
+
+  MyPlayedQuote(): Quote | null {
+    return this.Model.PlayedQuotes.find( x => x.PlayerName == this.Me.Name );
+  }
+
+}
